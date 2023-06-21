fcc128fea509b1692ead29b4bf491d0c84937645
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-06-21 22:26:22 +0300

    ыфвфывыф

---
+++ Charger220.java
@@ -0,0 +1,5 @@
+public class Charger220 {
+public void charge() {
+System.out.println("Зарядное устройство работает на 220 вольт");
+}
+}
---
+++ Voltage380.java
@@ -0,0 +1,5 @@
+public class Voltage380 {
+public int getVoltage() {
+return 380;
+}
+}
